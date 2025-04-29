925eaaeb99e4a48bbf8841ecc4bb0f91dd77b601
abialmiron <abialmiron@gmail.com>
2025-04-29 18:30:58 -0300

    conexion api

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header/Header';
 import { Routes } from 'react-router-dom';
+import Consumo from './pages/listaCartas';
 
 function App() {
   return (
     <div className='min-h-screen flex flex-col'>
       <Header />
-
+      <Consumo />
       <main className='flex-grow'>
         <Routes>
           {/** Acá irían las rutas */}
         </Routes>
       </main>
-
+        
       {/* Más adelante iría el Footer */}
     </div>
   );
